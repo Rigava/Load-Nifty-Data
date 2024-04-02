179aceb367064c8336cbf8c1ebaf68b95302c8e2
Rigava <josharun1@gmail.com>
2024-04-02 15:11:19 +0530

    10c

--- dashboard_online.py
+++ dashboard_online.py
@@ -35,11 +35,11 @@ from ctypes import *
 lib = CDLL("/home/appuser/lib/libta_lib.so.0.0.0")
 # import library
 try:
-    import talib-bin
+    import talib_bin
 except ImportError:
     subprocess.check_call([sys.executable, "-m", "pip", "install", "--global-option=build_ext", "--global-option=-L/home/appuser/lib/", "--global-option=-I/home/appuser/include/", "ta-lib-bin"])
 finally:
-    import talib-bin
+    import talib_bin
 
 import pandas as pd
 import plotly.graph_objects as go

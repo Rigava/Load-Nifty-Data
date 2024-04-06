f2dbbe73eb521325e619f267b965715888fe2039
Rigava <josharun1@gmail.com>
2024-04-06 15:47:44 +0530

    nf

--- dashboard_online.py
+++ dashboard_online.py
@@ -68,7 +68,7 @@ if dashboard == "Scanner":
     st.title(symbol+" Stocks Price Update")
     if symbol:
         try:
-            stock_url=f'https://www.nseindia.com/api/historical/cm/equity?symbol={symbol}'
+            stock_url=f'https://www.nseindia.com/api/historical/cm/equity?symbol=ITC'
             headers= {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36' ,
             "accept-encoding": "gzip, deflate, br", "accept-language": "en-US,en;q=0.9"}
             r = requests.get(stock_url, headers=headers).json()

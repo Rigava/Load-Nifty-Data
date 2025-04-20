d9affecf74ba5925048531b1db9f4f85d742b75b
Rigava <josharun1@gmail.com>
2025-04-20 15:36:59 +0530

    1a

--- dashboard_online.py
+++ dashboard_online.py
@@ -83,7 +83,7 @@ if dashboard == "Data":
         try:
             ticker = symbol+'.NS'
             stock_data = yfinance.Ticker(ticker).history(period="5y")
-            stock_data.index = stock_data.index.astype('datetime64[s]')
+            # stock_data.index = stock_data.index.astype('datetime64[s]')
             latest_price = stock_data['Close'].iloc[-1].round(1)
             latest_rsi = stock_data['RSI'].iloc[-1].round(1)
             st.success(f"The latest price is: {latest_price} and the rsi is {latest_rsi}")
